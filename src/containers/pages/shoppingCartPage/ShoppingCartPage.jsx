f41ad1b1d40c3f8fbee626e4926fa9d3d9ce4460
xuanthuy78 <phamthixuanthuy78@gmail.com>
2020-05-07 10:07:03 +0700

    finish shoppingCartPage

--- src/containers/pages/shoppingCartPage/ShoppingCartPage.jsx
+++ src/containers/pages/shoppingCartPage/ShoppingCartPage.jsx
@@ -3,6 +3,7 @@ import { MasterLayout } from '../../../components/layout'
 import './ShoppingCartPage.scss'
 import Partner from '../../../components/product/partner/Partner'
 import { Link } from 'react-router-dom'
+import ShoppingIcon from '../../../components/shoppingCart/shoppingIcon/ShoppingIcon'
 
 export class ShoppingCartPage extends Component {
   render() {
@@ -14,35 +15,7 @@ export class ShoppingCartPage extends Component {
               <span>Giỏ hàng của tôi</span>
             </p>
           </div>
-          <div className="shopping-icon">
-            <div className="item icon-cart active">
-              <div className="block">
-                <i className="fa fa-shopping-cart" aria-hidden="true"></i>
-              </div>
-              <div className="block"> Giỏ hàng của tôi</div>
-              <div className="block circle">
-                <span className="number">1</span>
-              </div>
-            </div>
-            <div className="item icon-pay">
-              <div className="block">
-                <i className="fa fa-usd" aria-hidden="true"></i>
-              </div>
-              <div className="block">Thanh toán</div>
-              <div className="block circle">
-                <span className="number">2</span>
-              </div>
-            </div>
-            <div className="item icon-finish">
-              <div className="block">
-                <i className="fa fa-check" aria-hidden="true"></i>
-              </div>
-              <div className="block"> Hoàn tất</div>
-              <div className="block circle">
-                <span className="number">3</span>
-              </div>
-            </div>
-          </div>
+          <ShoppingIcon />
           <div className="item shopping-content">
             <div className="table-product table-responsive">
               <table className="table content-table">
