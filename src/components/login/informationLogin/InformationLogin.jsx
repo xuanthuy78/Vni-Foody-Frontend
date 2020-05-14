2300916962e7f974024c1bdbcb0f0a4a06066dc3
vuaho <thivua98@gmail.com>
2020-05-14 15:03:11 +0700

    login page

--- src/components/login/informationLogin/InformationLogin.jsx
+++ src/components/login/informationLogin/InformationLogin.jsx
@@ -8,24 +8,24 @@ export class InformationLogin extends Component {
     return (
        <div className="login-content" >
         <h1 className="title"><span>Đăng nhập hệ thống</span></h1>
-        <div className="col-md-6  col-xs-12 col-sm-12 ">
-          <form className="form-inline">
+        <div className=" ">
+          <form className="form-horizontal">
             <div className="form-group">
-              <label  class="col-sm-4 control-label">Email</label>
-              <div className="col-sm-8">
+              <label className="col-xl-5 col-lg-5 col-md-5 col-sm-5 col-5 control-label">Email</label>
+              <div className="col-xl-7 col-lg-7 col-md-7 col-sm-7 col-7">
                 <input type="email" className="form-control" required="required" />
               </div>
             </div>
             <div className="form-group">
-              <label  className="col-sm-4 control-label">Mật khẩu</label>
-              <div className="col-sm-8">
+              <label  className="col-xl-5 col-lg-5 col-md-5 col-sm-5 col-5 control-label">Mật khẩu</label>
+              <div className="col-xl-7 col-lg-7 col-md-7 col-sm-7 col-7">
                 <input type="password" className="form-control"  required="required" />
               </div>
             </div>
             <div className="form-group">
-              <div className="col-sm-8 button-login">
+              <div className=" offset-5 col-xl-7 col-lg-7 col-md-7 col-sm-7 col-7 button-login">
                 <button type="submit" className="btn btn-primary">Đăng nhập</button>
-                <Link href="/quen-mat-khau.html" className="forgetpassword">Quên mật khẩu?</Link>
+                <Link to="#" className=" forgetpassword">Quên mật khẩu?</Link>
               </div>
             </div>
           </form>
