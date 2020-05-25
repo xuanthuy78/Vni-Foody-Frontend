dd05c5834e904213c504ed58d22cc97dd028f936
xuanthuy78 <phamthixuanthuy78@gmail.com>
2020-05-25 13:33:43 +0700

    modal product in adminPage

--- src/containers/pages/admin/productAdminPage/ProductAdminPage.jsx
+++ src/containers/pages/admin/productAdminPage/ProductAdminPage.jsx
@@ -1,52 +1,8 @@
 import React, { Component } from 'react'
 import './ProductAdminPage.scss'
 import MasterLayoutAdmin from '../../../../components/admin/layout/masterLayoutAdmin/MasterLayoutAdmin'
-import { Table, Space } from 'antd'
+import { Table, Space, Modal } from 'antd'
 import { Link } from 'react-router-dom'
-
-const columns = [
-  {
-    title: 'ID',
-    dataIndex: 'id',
-    key: 'id',
-  },
-  {
-    title: 'Image',
-    dataIndex: 'image',
-    key: 'image',
-  },
-  {
-    title: 'Name',
-    dataIndex: 'name',
-    key: 'name',
-    render: (text) => <Link to="#">{text}</Link>,
-  },
-  {
-    title: 'Type',
-    dataIndex: 'type',
-    key: 'type',
-  },
-  {
-    title: 'Number',
-    dataIndex: 'number',
-    key: 'number',
-  },
-  {
-    title: 'Describe',
-    dataIndex: 'describe',
-    key: 'describe',
-    render: (text) => <div className="describe">{text}</div>,
-  },
-  {
-    title: 'Action',
-    key: 'action',
-    render: (text, record) => (
-      <Space size="middle">
-        <Link to="#">Delete</Link>
-      </Space>
-    ),
-  },
-]
 
 const data = [
   {
@@ -62,7 +18,95 @@ const data = [
 ]
 
 export class ProductAdminPage extends Component {
+  constructor() {
+    super()
+    this.state = {
+      dataSource: null,
+      columns: null,
+      visible: false,
+    }
+  }
+  componentDidMount() {
+    setTimeout(() => {
+      this.buildColumsFromDatasource(data)
+    }, 1000)
+  }
+
+  showModal = () => {
+    this.setState({
+      visible: true,
+    })
+  }
+
+  handleOk = (e) => {
+    console.log(e)
+    this.setState({
+      visible: false,
+    })
+  }
+
+  handleCancel = (e) => {
+    console.log(e)
+    this.setState({
+      visible: false,
+    })
+  }
+
+  buildColumsFromDatasource(dataSource) {
+    const columns = [
+      {
+        title: 'ID',
+        dataIndex: 'id',
+        key: 'id',
+      },
+      {
+        title: 'Image',
+        dataIndex: 'image',
+        key: 'image',
+      },
+      {
+        title: 'Name',
+        dataIndex: 'name',
+        key: 'name',
+        width: 150,
+        render: (text) => <Link to="#">{text}</Link>,
+      },
+      {
+        title: 'Type',
+        dataIndex: 'type',
+        key: 'type',
+      },
+      {
+        title: 'Number',
+        dataIndex: 'number',
+        key: 'number',
+      },
+      {
+        title: 'Describe',
+        dataIndex: 'describe',
+        key: 'describe',
+        render: (text) => <div className="describe">{text}</div>,
+      },
+      {
+        title: 'Action',
+        key: 'action',
+        render: (text, record) => (
+          <Space size="middle" className="icon-btn">
+            <button type="button" className="btn btn-info">
+              <i className="fa fa-pencil-square-o" aria-hidden="true"></i>
+            </button>
+            <button type="button" className="btn btn-danger">
+              <i className="fa fa-trash-o" aria-hidden="true"></i>
+            </button>
+          </Space>
+        ),
+      },
+    ]
+    this.setState({ dataSource, columns })
+  }
+
   render() {
+    const { dataSource, columns } = this.state
     return (
       <MasterLayoutAdmin>
         <div className="main-detail">
@@ -100,16 +144,30 @@ export class ProductAdminPage extends Component {
                 </form>
               </div>
               <div className="nav-item add-master">
-                <Link type="submit" className="btn btn-warm" to="#">
+                <button
+                  type="submit"
+                  className="btn btn-warm"
+                  onClick={this.showModal}
+                >
                   <i className="fa fa-plus mr-2" aria-hidden="true"></i>
                   <span className="title-add">Add</span>
-                </Link>
+                </button>
               </div>
             </div>
           </div>
           <div className="table">
-            <Table columns={columns} dataSource={data} />
+            <Table columns={columns} dataSource={dataSource} />
           </div>
+          <Modal
+            title="Add product"
+            visible={this.state.visible}
+            onOk={this.handleOk}
+            onCancel={this.handleCancel}
+          >
+            <p>Some contents...</p>
+            <p>Some contents...</p>
+            <p>Some contents...</p>
+          </Modal>
         </div>
       </MasterLayoutAdmin>
     )
