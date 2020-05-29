34b21af8e1ea2241cc12ea82acd1f4aa9b3e4fff
vuaho <thivua98@gmail.com>
2020-05-29 08:59:54 +0700

    productdetail page

--- src/components/productdetail/aboutInformationProduct/AboutInformationProduct.jsx
+++ src/components/productdetail/aboutInformationProduct/AboutInformationProduct.jsx
@@ -87,70 +87,67 @@ export class AboutInformationProduct extends Component {
               </div>
             </div>
             <div className="button-card">
-              <div className="modal-content">
-                <Link to="#" className="btn btn-dark" onClick={this.showModal}>
-                  <i className="fa fa-shopping-cart" aria-hidden="true"></i>{' '}
-                  Thêm giỏ hàng
-                </Link>
-                <Modal
-                  size="lg"
-                  title=" Bạn có 2 sản phẩm trong giỏ hàng."
-                  visible={this.state.visible}
-                  onOk={this.handleOk}
-                  onCancel={this.handleCancel}
-                >
-                  <div className="modal-body">
-                    <table className="table">
-                      <thead>
-                        <tr>
-                          <th />
-                          <th>Tên sản phẩm</th>
-                          <th>Số lượng</th>
-                          <th>Giá tiền</th>
-                          <th />
-                        </tr>
-                      </thead>
-                      <tbody>
-                        <tr
-                          className="line-item ng-scope"
-                          ng-repeat="item in OrderDetails"
-                        >
-                          <td className="item-image">
-                            <img
-                              className="img-top"
-                              src={'assets/images/sup_cua_large.jpg'}
-                              alt=""
-                              width="100px"
-                              height="50px"
-                            />
-                          </td>
-                          <td className="item-title">
-                            <Link
-                              to="/san-pham/sup-ga-kem-sua.html"
-                              className="ng-binding"
-                            >
-                              Súp gà kem sữa
-                              <br />
-                            </Link>
-                          </td>
-                          <td className="item-quantity">
-                            <input
-                              type="number"
-                              className="text ng-pristine ng-untouched ng-valid"
-                              ng-model="item.Quantity"
-                              ng-change="updateItemCart(item)"
-                            />
-                          </td>
-                          <td className="item-price ng-binding">312,000₫</td>
-                          <td className="item-delete">
-                            <Link to="/#">Xóa</Link>
-                          </td>
-                        </tr>
-                      </tbody>
-                    </table>
-                  </div>
-                </Modal>
-              </div>
+              <Link to="#" className="btn btn-dark" onClick={this.showModal}>
+                <i className="fa fa-shopping-cart" aria-hidden="true"></i> Thêm
+                giỏ hàng
+              </Link>
+              <Modal
+                title=" Bạn có 2 sản phẩm trong giỏ hàng."
+                visible={this.state.visible}
+                onOk={this.handleOk}
+                onCancel={this.handleCancel}
+              >
+                <div className="modal-body">
+                  <table className="table">
+                    <thead>
+                      <tr>
+                        <th />
+                        <th>Tên sản phẩm</th>
+                        <th>Số lượng</th>
+                        <th>Giá tiền</th>
+                        <th />
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr
+                        className="line-item ng-scope"
+                        ng-repeat="item in OrderDetails"
+                      >
+                        <td className="item-image">
+                          <img
+                            className="img-top"
+                            src={'assets/images/sup_cua_large.jpg'}
+                            alt=""
+                            width="100px"
+                            height="50px"
+                          />
+                        </td>
+                        <td className="item-title">
+                          <Link
+                            to="/san-pham/sup-ga-kem-sua.html"
+                            className="ng-binding"
+                          >
+                            Súp gà kem sữa
+                            <br />
+                          </Link>
+                        </td>
+                        <td className="item-quantity">
+                          <input
+                            type="number"
+                            className="text ng-pristine ng-untouched ng-valid"
+                            ng-model="item.Quantity"
+                            ng-change="updateItemCart(item)"
+                          />
+                        </td>
+                        <td className="item-price ng-binding">312,000₫</td>
+                        <td className="item-delete">
+                          <Link to="/#">Xóa</Link>
+                        </td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </div>
+              </Modal>
               <Link to="#" className="btn btn-danger">
                 <i className="fa fa-check" aria-hidden="true"></i> Mua ngay
               </Link>
