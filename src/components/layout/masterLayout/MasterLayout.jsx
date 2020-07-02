4c6582973b52c422c191aff104a97845c7adf791
xuanthuy78 <phamthixuanthuy78@gmail.com>
2020-07-02 09:42:00 +0700

    backtop

--- src/components/layout/masterLayout/MasterLayout.jsx
+++ src/components/layout/masterLayout/MasterLayout.jsx
@@ -1,24 +1,7 @@
 import React, { Component } from 'react'
 import { Header, Footer } from '../index'
+import { BackTop } from 'antd'
 export class MasterLayout extends Component {
-  constructor(props) {
-    super(props)
-
-    this.state = {
-      intervalId: 0,
-    }
-  }
-  scrollStep() {
-    if (window.pageYOffset === 0) {
-      clearInterval(this.state.intervalId)
-    }
-    window.scroll(0, window.pageYOffset - 50)
-  }
-  scrollToTop = () => {
-    let intervalId = setInterval(this.scrollStep.bind(this), 16.66)
-    this.setState({ intervalId: intervalId })
-  }
-
   render() {
     const childrenWithProps = React.Children.map(this.props.children, (child) =>
       React.cloneElement(child, {})
@@ -29,9 +12,11 @@ export class MasterLayout extends Component {
         <main className="">{childrenWithProps}</main>
         <Footer></Footer>
         <div className="back-to-top" onClick={this.scrollToTop}>
-          <button className="btn-top" id="myBtn" title="Go to top">
-            <i className="fa fa-angle-up"></i>
-          </button>
+          <BackTop>
+            <button className="btn-top" id="myBtn" title="Go to top">
+              <i className="fa fa-angle-up"></i>
+            </button>
+          </BackTop>
         </div>
       </div>
     )
